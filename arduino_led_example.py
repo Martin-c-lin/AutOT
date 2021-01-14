92b892b07ceb6feaf1f021f289009a19208f3dc6
Martin <martin1995@live.se>
2021-01-14 10:14:06 +0100

    Started on simple control script for the LED via arduino.

---
+++ arduino_led_example.py
@@ -0,0 +1,26 @@
+import serial, time
+from random import randint
+ArduinoUnoSerial = serial.Serial('com3',9600)
+
+#print(ArduinoUnoSerial.readline())
+
+print ("You have new message from Arduino")
+running = True
+
+while running:         #Do this forever
+    #var = randint(0,1) #raw_input()                                          #get input from user
+    #var = str(var)
+    var = input("Please enter a string:\n")
+    print(var)
+    ArduinoUnoSerial.write(1)
+    if (var == '1'):                                                #if the value is 1
+        ArduinoUnoSerial.write(b'H')                      #send 1 to the arduino's Data code
+        print ("LED turned ON")
+        time.sleep(0.1)
+    if (var == '0'):
+        ArduinoUnoSerial.write(b'L')            #send 0 to the arduino's Data code
+        print ("LED turned OFF")
+        time.sleep(0.1)
+    if (var == 'quit'): #if the answer is (fine and you)
+        ArduinoUnoSerial.close()
+        running = False
---
+++ control_led_python/control_led_python.ino
@@ -0,0 +1,26 @@
+int incomingByte;
+const int LED=13;
+void setup() { 
+  Serial.begin(9600);                               //initialize serial COM at 9600 baudrate
+  pinMode(LED, OUTPUT);                    //declare the LED pin (13) as output
+  digitalWrite(LED, HIGH);
+  delay(500);
+  Serial.println("Hello!,How are you Python ?");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'H') {
+      digitalWrite(ledPin, HIGH);
+    }
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'L') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
